77b58b7b73b20a555626bb2913b92f1f6449afb5
Jamie Lin <jamie_lin@adata.com>
2021-11-11 16:28:46 +0800

    feat: initialize firebase

---
+++ configs/firebase.tsx
@@ -0,0 +1,6 @@
+import { FirebaseOptions } from '@firebase/app'
+
+export const config: FirebaseOptions = {
+  apiKey: 'AIzaSyDXOnEwTTkXfv7v6YXBw7mzG2FQPhp0J4A',
+  projectId: 'fir-demo-69b11',
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
+import { getApps, initializeApp } from '@firebase/app'
 import type { AppProps } from 'next/app'
+import { config } from '../configs/firebase'
 import AuthProvider from '../providers/auth'
 
 const App = ({ Component, pageProps }: AppProps) => {
+  if (getApps().length === 0) {
+    initializeApp(config)
+  }
+
   return (
     <AuthProvider>
       <Component {...pageProps} />
